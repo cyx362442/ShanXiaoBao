452fa0ff223fdb97cd3a234e19053d7a414a94f6
cyx362442 <chenyunyan88@aliyun.com>
2017-04-27 22:52:04 +0800

    sxb04275

--- app/src/main/java/com/zhongbang/sxb/webview/Link2Activity.java
+++ app/src/main/java/com/zhongbang/sxb/webview/Link2Activity.java
@@ -87,6 +87,8 @@ public class Link2Activity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_link2);
         SharedPreferences sp = getSharedPreferences("users", MODE_PRIVATE);
         mPhone = sp.getString("name", "");
+
+
         image_rotating = (ImageView) findViewById(R.id.image_load);
         layout_rotating = findViewById(R.id.rl_load);
         startAnim();
